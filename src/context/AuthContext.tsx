d2b293eb70692f7e54c8c296f66d22e577d2ad8a
Ananda Afryan Gemilang <anandalark007@gmail.com>
2025-10-14 07:58:15 +0700

    feat: handle logout redirect to login

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import type { ReactNode } from "react";
+import { jwtDecode } from "jwt-decode";
+
 import { AuthContext } from "../hooks/useAuth";
 
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
@@ -14,6 +16,29 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
     localStorage.removeItem("token");
     setToken(null);
   };
+
+  const scheduleAutoLogout = (token: string, logout: () => void) => {
+    try {
+      const decoded: { exp: number } = jwtDecode(token);
+      const expiresInMs = decoded.exp * 1000 - Date.now();
+
+      if (expiresInMs > 0) {
+        setTimeout(() => {
+          logout();
+        }, expiresInMs);
+      } else {
+        logout();
+      }
+    } catch {
+      logout();
+    }
+  }
+
+  useEffect(() => {
+    if (token) {
+      scheduleAutoLogout(token, logout);
+    }
+  }, [token]);
 
   return (
     <AuthContext.Provider value={{ token, login, logout, isAuthenticated: !!token }}>
